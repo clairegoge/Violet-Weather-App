be479336255a9a4504e9710f98d5d2dd152b00c9
clairegoge <nonigoge@gmail.com>
2026-07-13 09:20:43 +0200

    updated the degrees metric

--- src/index.js
+++ src/index.js
@@ -80,9 +80,9 @@ function displayForecast(response) {
             <div ><img src="${day.condition.icon_url}" class="weather-forecast-icon"/></div>
             <div class="weather-forecast-temperatures">
               <div class="weather-forecast-temperature">
-                <strong>${Math.round(day.temperature.maximum)}° </strong>
+                <strong>${Math.round(day.temperature.maximum)}°C </strong>
               </div>
-              <div class="weather-forecast-temperature">${Math.round(day.temperature.minimum)}°</div>
+              <div class="weather-forecast-temperature">${Math.round(day.temperature.minimum)}°C</div>
             </div>
           </div>`;
     }
